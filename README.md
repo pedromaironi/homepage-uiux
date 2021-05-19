7c97d2996f734214129e2c02b5fb17c36e0cf687
pedromaironi <pedrom_t@hotmail.com>
2021-05-19 20:13:51 +0200

    Setup project

---
+++ README.md
@@ -0,0 +1,43 @@
+# undefined
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+
+## About <a name = "about"></a>
+
+This is a responsive page for practice.
+
+## Getting Started <a name = "getting_started"></a>
+
+Install the prerequisites
+
+### Prerequisites
+
+What things you need to install the software and how to install them.
+
+Install via NPM
+Install the boxicons package by copy pasting the following in your terminal:
+
+```
+$ npm install boxicons --save
+```
+
+Import the npm module in your javascript
+
+```
+import 'boxicons'
+```
+
+Install Scroll reveal or use npm
+
+### Package
+
+```
+npm install scrollreveal --save
+```
+
+```
+<script src="https://unpkg.com/scrollreveal"></script>
+```
---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+import 'boxicons';
+import 'scrollreveal';
+
+/*==================== SHOW MENU ====================*/
+
+    // Validate that variables exist
+
+            // We add the show-menu class to the div tag with the nav__menu class
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  
+
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    
+    // We save the theme and the current icon that the user chose
+    
+})
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
