4f400decc773a73094e03f686b52c773d536a3a0
pedromaironi <pedrom_t@hotmail.com>
2021-05-21 18:49:43 +0200

    Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-import 'boxicons';
-import 'scrollreveal';
+// import 'boxicons';
+// import 'scrollreveal';
 
 /*==================== SHOW MENU ====================*/
 
@@ -7,14 +7,57 @@ import 'scrollreveal';
 
             // We add the show-menu class to the div tag with the nav__menu class
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navId);
 
+  if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show-menu');
+      });
+  }
+};
+showMenu('nav-toggle','nav-menu');
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
 
+window.addEventListener('scroll', scrollHeader);
 /*==================== SHOW SCROLL TOP ====================*/ 
 
 
@@ -32,11 +75,11 @@ import 'scrollreveal';
 
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
+// themeButton.addEventListener('click', () => {
+//     // Add or remove the dark / icon theme
     
-    // We save the theme and the current icon that the user chose
+//     // We save the theme and the current icon that the user chose
     
-})
+// })
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
